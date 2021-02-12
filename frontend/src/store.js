f1dc608668c2678cb55c38ab80f81b769f43f90a
Sanchit-Soni <sanchitsony@gmail.com>
2021-02-13 01:12:18 +0530

    Setting Up REDUX

---
+++ frontend/src/store.js
@@ -0,0 +1,24 @@
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+import {
+  productListReducer,
+  productDetailsReducer,
+} from "./reducers/productReducers";
+
+const reducer = combineReducers({
+  productList: productListReducer,
+  productDetails: productDetailsReducer,
+});
+
+const initialStore = {};
+
+const middleware = [thunk];
+
+const store = createStore(
+  reducer,
+  initialStore,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
+
+export default store;
