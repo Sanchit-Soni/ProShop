a8d65209ae3ab9ab75d96d8be634ab10cde3792b
Sanchit-Soni <sanchitsony@gmail.com>
2021-05-21 16:55:43 +0530

    admin screen products

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,18 +1,27 @@
 import express from "express";
+import { protect, admin } from "../middleware/authMiddleware.js";
+
 const router = express.Router();
 import {
+  deleteProduct,
   getProductById,
   getProducts,
+  updateProduct,
+  createProduct,
 } from "../controllers/productController.js";
 
 // @desc Fetch all products
 // @route GET /api/products
 // @access Public
-router.route("/").get(getProducts);
+router.route("/").get(getProducts).post(protect, admin, createProduct);
 
 // @desc Fetch single products
 // @route GET /api/products/:id
 // @access Public
-router.route("/:id").get(getProductById);
+router
+  .route("/:id")
+  .get(getProductById)
+  .delete(protect, admin, deleteProduct)
+  .put(protect, admin, updateProduct);
 
 export default router;
