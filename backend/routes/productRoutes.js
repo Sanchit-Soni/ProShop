7720e4e463d5296e04e57c4861707fcf9754fd60
Sanchit-Soni <sanchitsony@gmail.com>
2021-06-01 01:26:05 +0530

    review backend

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -8,12 +8,14 @@ import {
   getProducts,
   updateProduct,
   createProduct,
+  createProductReview,
 } from "../controllers/productController.js";
 
 // @desc Fetch all products
 // @route GET /api/products
 // @access Public
 router.route("/").get(getProducts).post(protect, admin, createProduct);
+router.route("/:id/reviews").post(protect, createProductReview);
 
 // @desc Fetch single products
 // @route GET /api/products/:id
