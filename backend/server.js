bfb628c6fcb5154e9eb026f16115b9713fe87438
Sanchit-Soni <sanchitsony@gmail.com>
2021-06-06 02:04:10 +0530

    test

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ import colors from "colors";
 import morgan from "morgan";
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
-import orderRoutes from "./routes/OrderRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
 import uploadRoutes from "./routes/uploadRoutes.js";
 
 dotenv.config();
